d75ecd3203ee2735ca8cecef52b48070575a3489
Max Milton <max@wearegenki.com>
2018-07-08 08:52:26 +1000

    Update collapse component tests

--- components/collapse/test/MinnaCollapse.test.js
+++ components/collapse/test/MinnaCollapse.test.js
@@ -18,12 +18,12 @@ describe('MinnaCollapse component', () => {
     expect.assertions(4);
     const target = document.createElement('div');
     const component = new MinnaCollapse({ target });
-    expect(component.get().__show).toEqual(false);
+    expect(component.get().__isOpen).toBeFalsy();
     const button = target.querySelector('button');
     button.click();
-    expect(component.get().__show).toEqual(true);
+    expect(component.get().__isOpen).toBeTruthy();
     button.click();
-    expect(component.get().__show).toEqual(false);
+    expect(component.get().__isOpen).toBeFalsy();
     expect(document.querySelector('collapse-hide')).toBeNull();
   });
 });

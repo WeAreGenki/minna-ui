25794ce162411fb4c093c587decb500597024562
Max Milton <max@wearegenki.com>
2018-09-01 20:57:49 +1000

    Update tests

--- utils/build-component/component-externs.js
+++ utils/build-component/component-externs.js
@@ -18,6 +18,7 @@ var m = () => {};
 // component exports
 // XXX: Workaround until upstream bug is fixed:
 // https://github.com/ampproject/rollup-plugin-closure-compiler/issues/49
+var TestComponent = () => {};
 var MinnaCodeView = () => {};
 var MinnaCollapse = () => {};
 var MinnaNavbar = () => {};

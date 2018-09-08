24eebc0fae3a9e212c2250ed3f3263e717cbb8a6
Max Milton <max@wearegenki.com>
2018-09-08 12:17:26 +1000

    Remove workaround for exters

--- utils/build-component/component-externs.js
+++ utils/build-component/component-externs.js
@@ -8,16 +8,3 @@
 // Svelte quoted method references
 var i = () => {};
 var m = () => {};
-
-// component exports
-// XXX: Workaround until upstream bug is fixed:
-// https://github.com/ampproject/rollup-plugin-closure-compiler/issues/49
-var TestComponent = () => {};
-var MinnaCodeView = () => {};
-var MinnaCollapse = () => {};
-var MinnaNavbar = () => {};
-var MinnaSelect = () => {};
-var MinnaSwitch = () => {};
-var MinnaTabs = () => {};
-var MinnaToasts = () => {};
-var MinnaTooltip = () => {};

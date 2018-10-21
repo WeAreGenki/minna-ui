c954e8855ecda6dbd993db14675e6f4ba712e75c
Max Milton <max@wearegenki.com>
2018-10-21 18:22:23 +1100

    New way of doing PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,10 @@
 
 'use strict';
 
-const minnaUi = require('@minna-ui/postcss-config');
-
 module.exports = {
   parser: 'postcss-scss', // FIXME: `//` comments are not being converted to block
   map: true,
-  plugins: [
-    minnaUi({ debug: true }),
-  ],
+  plugins: {
+    '@minna-ui/postcss-config': { debug: true },
+  },
 };

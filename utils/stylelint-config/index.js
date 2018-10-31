7f940d52f0c04053a8115de54535185cafb4cbcf
Max Milton <max@wearegenki.com>
2018-11-01 07:26:32 +1100

    Tweak StyleLint config so a11y is less in your face

--- utils/stylelint-config/index.js
+++ utils/stylelint-config/index.js
@@ -18,17 +18,17 @@ module.exports = {
     'stylelint-order',
   ],
   rules: {
-    'a11y/media-prefers-reduced-motion': true,
-    'a11y/no-outline-none': true,
-    'a11y/selector-pseudo-class-focus': true,
     'a11y/content-property-no-static-value': [true, { severity: 'warning' }],
     'a11y/font-size-is-readable': [true, { severity: 'warning' }],
     'a11y/line-height-is-vertical-rhythmed': [true, { severity: 'warning' }],
+    'a11y/media-prefers-reduced-motion': [true, { severity: 'warning' }],
     'a11y/no-display-none': [true, { severity: 'warning' }],
-    'a11y/no-spread-text': [true, { severity: 'warning' }],
     'a11y/no-obsolete-attribute': [true, { severity: 'warning' }],
     'a11y/no-obsolete-element': [true, { severity: 'warning' }],
+    'a11y/no-outline-none': true,
+    'a11y/no-spread-text': [true, { severity: 'warning' }],
     'a11y/no-text-align-justify': [true, { severity: 'warning' }],
+    'a11y/selector-pseudo-class-focus': true,
     'at-rule-no-unknown': [true, {
       ignoreAtRules: [
         'content',

e656ed4ffa23696dd6df2a3747f72ffcd18c175d
Max Milton <max@wearegenki.com>
2018-11-11 12:22:24 +1100

    Ignore files from test coverage

--- utils/meta/index.js
+++ utils/meta/index.js
@@ -1,3 +1,7 @@
+/* istanbul ignore file */
+
+'use strict'; // eslint-disable-line
+
 const preMarkup = require('@minna-ui/pre-markup');
 const preStyle = require('@minna-ui/pre-style');
 const { catchErr, makeCss, makeHtml } = require('@minna-ui/rollup-plugins');

e0e7acb93ce894bedaac444a5642b048e6a85ef4
Max Milton <max@wearegenki.com>
2018-12-24 07:14:22 +1100

    Generate source map in CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
+/* tslint:disable object-literal-sort-keys */
+
 'use strict';
 
 module.exports = {
   syntax: 'postcss-scss',
-
+  map: true,
   plugins: {
     '@minna-ui/postcss-config': { debug: true },
   },

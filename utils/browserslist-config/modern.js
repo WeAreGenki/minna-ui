4ce2ffccba37c021b7d801f9e9d188429033a404
Max Milton <max@wearegenki.com>
2019-01-26 10:53:01 +1100

    Add new browserslist presets package

---
+++ utils/browserslist-config/modern.js
@@ -0,0 +1,17 @@
+// https://github.com/browserslist/browserslist
+
+'use strict';
+
+/**
+ * Supports popular modern, evergreen browsers.
+ */
+module.exports = [
+  '> 2%',
+  'last 2 Chrome versions',
+  'last 2 ChromeAndroid versions',
+  'last 2 Edge versions',
+  'last 2 Firefox versions',
+  'last 2 iOS versions',
+  'last 2 Safari versions',
+  'not dead',
+];
---
+++ utils/browserslist-config/node.js
@@ -0,0 +1,10 @@
+// https://github.com/browserslist/browserslist
+
+'use strict';
+
+/**
+ * Supports the latest node LTS version.
+ * Kept up to date with release schedule:
+ * @see {@link https://github.com/nodejs/Release#release-schedule}
+ */
+module.exports = ['node 10'];

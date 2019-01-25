401fbb631e6c93a9be2891ffddad5cd638c839ab
Max Milton <max@wearegenki.com>
2019-01-26 10:54:54 +1100

    Improve prettier config preset

--- utils/prettier-config/index.js
+++ utils/prettier-config/index.js
@@ -1,35 +1,48 @@
 // https://prettier.io/docs/en/options.html
 
+/* eslint-disable jsdoc/valid-types */
+
 'use strict';
 
 const merge = require('deepmerge');
 
 /**
  * Minna UI prettier config preset.
- * @param {?object} opts Custom prettier options to merge with Minna UI preset.
+ * @param {import('prettier').Options=} opts Custom user defined prettier
+ * options to merge with Minna UI preset.
+ * @returns {import('prettier').Options}
  */
-module.exports = (opts = {}) =>
-  merge(
-    {
-      endOfLine: 'lf',
-      singleQuote: true,
-      trailingComma: 'all',
+module.exports = (opts = {}) => merge(
+  {
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    singleQuote: true,
+    trailingComma: 'all',
 
-      overrides: [
-        {
-          files: '*.html',
-          options: {
-            htmlWhitespaceSensitivity: 'ignore',
-            requirePragma: true,
-          },
+    overrides: [
+      {
+        files: '*.html',
+        options: {
+          // TODO: Good for svelte but horrible for everything else, can it
+          // be conditional depending on the file?
+          // htmlWhitespaceSensitivity: 'ignore',
+          requirePragma: true,
         },
-        {
-          files: ['*.test.js', '*.test.jsx', '*.test.ts', '*.test.tsx'],
-          options: {
-            printWidth: 100,
-          },
+      },
+      {
+        files: [
+          '*.test.ts',
+          '*.test.tsx',
+          '*.test.js',
+          '*.test.jsx',
+          '*.spec.ts',
+          '*.spec.js',
+        ],
+        options: {
+          printWidth: 100,
         },
-      ],
-    },
-    opts,
-  );
+      },
+    ],
+  },
+  opts,
+);

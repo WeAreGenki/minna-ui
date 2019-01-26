697ec0640b66fe370d17efe0279c4a10672d1b8e
Max Milton <max@wearegenki.com>
2019-01-26 16:16:16 +1100

    Split tests into 2 files

--- utils/eslint-config/test/index.test.js
+++ utils/eslint-config/test/index.test.js
@@ -1,16 +1,16 @@
 /** @jest-environment node */
 
+// TODO: Add tests for node.js config
+
 'use strict';
 
-const path = require('path');
+// const { join } = require('path');
 const { CLIEngine } = require('eslint');
+const baseConfig = require('../index.js');
 
 const linterCli = new CLIEngine({
-  configFile: path.join(__dirname, '../index.js'),
-  useEslintrc: false,
-});
-const linterCliJest = new CLIEngine({
-  configFile: path.join(__dirname, '../jest.js'),
+  // configFile: join(__dirname, '..', 'index.js'),
+  baseConfig,
   useEslintrc: false,
 });
 
@@ -32,26 +32,6 @@ debugger;
 
 module.exports = output;
 `;
-const sourceJestValid = `/* eslint-env es6 */
-
-'use strict';
-
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-const { target } = require('@minna-ui/jest-config/fixtures/importable.js');
-
-test('target', () => {
-  expect(target()).toEqual('');
-});
-`;
-const sourceJestInvalid = `
-import config from '@minna-ui/jest-config';
-
-console.log(this);
-const output = target();
-debugger;
-
-export default output;
-`;
 
 describe('ESLint config', () => {
   it('runs without linting errors on valid JS', () => {
@@ -64,21 +44,6 @@ describe('ESLint config', () => {
   it('detects linting errors', () => {
     expect.assertions(1);
     const output = linterCli.executeOnText(sourceInvalid);
-    expect(output.errorCount).not.toEqual(0);
-  });
-});
-
-describe('Jest ESLint config', () => {
-  it('runs without linting errors on valid JS', () => {
-    expect.assertions(2);
-    const output = linterCliJest.executeOnText(sourceJestValid);
-    expect(output.errorCount).toEqual(0);
-    expect(output.warningCount).toEqual(0);
-  });
-
-  it('detects linting errors', () => {
-    expect.assertions(1);
-    const output = linterCliJest.executeOnText(sourceJestInvalid);
     expect(output.errorCount).not.toEqual(0);
   });
 });

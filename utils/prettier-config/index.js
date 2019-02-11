a8d6b7944443a7274c695813e842ea8259abe837
Max Milton <max@wearegenki.com>
2019-02-12 08:23:55 +1100

    Add .svelte file ext to prettier config

--- utils/prettier-config/index.js
+++ utils/prettier-config/index.js
@@ -1,6 +1,6 @@
 // https://prettier.io/docs/en/options.html
 
-/* eslint-disable jsdoc/valid-types */
+/* eslint-disable jsdoc/valid-types, sort-keys */
 
 'use strict';
 
@@ -12,37 +12,37 @@ const merge = require('deepmerge');
  * options to merge with Minna UI preset.
  * @returns {import('prettier').Options}
  */
-module.exports = (opts = {}) => merge(
-  {
-    arrowParens: 'always',
-    endOfLine: 'lf',
-    singleQuote: true,
-    trailingComma: 'all',
-
-    overrides: [
-      {
-        files: '*.html',
-        options: {
-          // TODO: Good for svelte but horrible for everything else, can it
-          // be conditional depending on the file?
-          // htmlWhitespaceSensitivity: 'ignore',
-          requirePragma: true,
+module.exports = (opts = {}) =>
+  merge(
+    {
+      arrowParens: 'always',
+      endOfLine: 'lf',
+      singleQuote: true,
+      trailingComma: 'all',
+      overrides: [
+        {
+          files: ['*.html', '*.svelte'],
+          options: {
+            // TODO: Good for svelte but horrible for everything else, can it
+            // be conditional depending on the file?
+            // htmlWhitespaceSensitivity: 'ignore',
+            requirePragma: true,
+          },
         },
-      },
-      {
-        files: [
-          '*.test.ts',
-          '*.test.tsx',
-          '*.test.js',
-          '*.test.jsx',
-          '*.spec.ts',
-          '*.spec.js',
-        ],
-        options: {
-          printWidth: 100,
+        {
+          files: [
+            '*.test.ts',
+            '*.test.tsx',
+            '*.test.js',
+            '*.test.jsx',
+            '*.spec.ts',
+            '*.spec.js',
+          ],
+          options: {
+            printWidth: 100,
+          },
         },
-      },
-    ],
-  },
-  opts,
-);
+      ],
+    },
+    opts,
+  );

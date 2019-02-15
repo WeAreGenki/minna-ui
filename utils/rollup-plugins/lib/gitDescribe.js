2a975de14a8bd3388769c54b9f28cfa85aaf318c
Max Milton <max@wearegenki.com>
2019-02-16 06:25:20 +1100

    Fix type

--- utils/rollup-plugins/lib/gitDescribe.js
+++ utils/rollup-plugins/lib/gitDescribe.js
@@ -6,17 +6,17 @@ const { execSync } = require('child_process');
 /**
  * Get the most recent git reference.
  * @see https://git-scm.com/docs/git-describe
- * @returns {(string|void)} A human readable git reference.
+ * @returns {string} A human readable git reference.
  */
 /* eslint-disable-next-line consistent-return */
 function gitDescribe() {
   try {
-    const reference = execSync(
-      'git describe --always --dirty="-dev"',
-    ).toString().trim();
+    const reference = execSync('git describe --always --dirty="-dev"')
+      .toString()
+      .trim();
     return reference;
   } catch (error) {
-    /* eslint-disable-next-line no-console */ /* tslint:disable-next-line no-console */
+    /* eslint-disable-next-line no-console */
     console.log(error);
   }
 }

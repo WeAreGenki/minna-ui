22bf0ae01c5dbdd73a603e10b7265bea6f1d4761
Max Milton <max@wearegenki.com>
2019-02-16 06:26:46 +1100

    Update file extensions

--- utils/minna-ui/index.esm.js
+++ utils/minna-ui/index.esm.js
@@ -1,13 +1,13 @@
 /* istanbul ignore file */ // FIXME: This isn't working
 
 /** Components */
-export { default as CodeView } from '@minna-ui/code-view/src/CodeView.html';
-export { default as Collapse } from '@minna-ui/collapse/src/Collapse.html';
-export { default as Navbar } from '@minna-ui/navbar/src/Navbar.html';
-export { default as Select } from '@minna-ui/select/src/Select.html';
-export { default as Switch } from '@minna-ui/switch/src/Switch.html';
-export { default as Tabs } from '@minna-ui/tabs/src/Tabs.html';
-export { default as Toast } from '@minna-ui/toast/src/ToastLauncher.html';
+export { default as CodeView } from '@minna-ui/code-view/src/CodeView.svelte';
+export { default as Collapse } from '@minna-ui/collapse/src/Collapse.svelte';
+export { default as Navbar } from '@minna-ui/navbar/src/Navbar.svelte';
+export { default as Select } from '@minna-ui/select/src/Select.svelte';
+export { default as Switch } from '@minna-ui/switch/src/Switch.svelte';
+export { default as Tabs } from '@minna-ui/tabs/src/Tabs.svelte';
+export { default as Toast } from '@minna-ui/toast/src/ToastLauncher.svelte';
 
 /** Actions */
 export { default as Tooltip } from '@minna-ui/tooltip-action';

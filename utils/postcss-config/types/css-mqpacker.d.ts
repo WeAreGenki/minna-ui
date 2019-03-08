3f121718396dbbaf5b5bfd50a7d8f688714550e4
Max Milton <max@wearegenki.com>
2019-03-09 09:26:41 +1100

    Generate types

---
+++ utils/postcss-config/types/css-mqpacker.d.ts
@@ -0,0 +1,7 @@
+export = index;
+declare function index(...args: any[]): any;
+declare namespace index {
+  function pack(css: any, opts: any): any;
+  const postcss: any;
+  function process(css: any, processOpts: any, pluginOpts: any): any;
+}
---
+++ utils/postcss-config/types/postcss-advanced-variables.d.ts
@@ -0,0 +1,6 @@
+export = index;
+declare function index(...args: any[]): any;
+declare namespace index {
+  const postcss: any;
+  function process(css: any, processOpts: any, pluginOpts: any): any;
+}

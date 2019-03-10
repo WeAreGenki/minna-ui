5c12c971e29fce234af7ba0c9cfc41b701d91d30
Max Milton <max@wearegenki.com>
2019-03-10 11:18:04 +1100

    Fix component module proxy

--- utils/minna-ui/index.mjs
+++ utils/minna-ui/index.mjs
@@ -1,13 +1,13 @@
-/* istanbul ignore file */ // FIXME: This isn't working
+// FIXME: Uncomment broken packages
 
-/** Components */
-export { default as CodeView } from '@minna-ui/code-view/src/CodeView.svelte';
-export { default as Collapse } from '@minna-ui/collapse/src/Collapse.svelte';
-export { default as Navbar } from '@minna-ui/navbar/src/Navbar.svelte';
-export { default as Select } from '@minna-ui/select/src/Select.svelte';
-export { default as Switch } from '@minna-ui/switch/src/Switch.svelte';
-export { default as Tabs } from '@minna-ui/tabs/src/Tabs.svelte';
-export { default as Toast } from '@minna-ui/toast/src/ToastLauncher.svelte';
+// Components
+export { default as CodeView } from '@minna-ui/code-view';
+export { default as Collapse } from '@minna-ui/collapse';
+export { default as Navbar } from '@minna-ui/navbar';
+export { default as Select } from '@minna-ui/select';
+export { default as Switch } from '@minna-ui/switch';
+// export { default as Tabs } from '@minna-ui/tabs';
+export { default as Toast } from '@minna-ui/toast';
 
-/** Actions */
-export { default as Tooltip } from '@minna-ui/tooltip-action';
+// Actions
+// export { default as Tooltip } from '@minna-ui/tooltip-action';

1345bde72d77cc7a11b995980ff35ca741fd3633
Max Milton <max@wearegenki.com>
2019-03-10 14:32:26 +1100

    Uncomment and fix exports

--- utils/minna-ui/index.mjs
+++ utils/minna-ui/index.mjs
@@ -6,8 +6,8 @@ export { default as Collapse } from '@minna-ui/collapse';
 export { default as Navbar } from '@minna-ui/navbar';
 export { default as Select } from '@minna-ui/select';
 export { default as Switch } from '@minna-ui/switch';
-// export { default as Tabs } from '@minna-ui/tabs';
+export { default as Tabs } from '@minna-ui/tabs';
 export { default as Toast } from '@minna-ui/toast';
 
 // Actions
-// export { default as Tooltip } from '@minna-ui/tooltip-action';
+export * from '@minna-ui/tooltip-action';

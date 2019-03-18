f26bb0773b16bae1b2a5d2850420c16f70cea6d0
Max Milton <max@wearegenki.com>
2019-03-19 08:02:48 +1100

    Fix type

--- utils/rollup-plugins/src/catchErr.ts
+++ utils/rollup-plugins/src/catchErr.ts
@@ -2,6 +2,6 @@
  * Generic error handler for nodejs callbacks.
  * @param err Error from node method callback.
  */
-export function catchErr(err: Error): void {
+export function catchErr(err?: Error): void {
   if (err) throw err;
 }

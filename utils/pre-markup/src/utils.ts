618e78fffe230fe0179fc294aea9f37f8b2aefca
Max Milton <max@wearegenki.com>
2019-03-23 07:15:25 +1100

    Add new html tagged template literal helper function

---
+++ utils/pre-markup/src/utils.ts
@@ -0,0 +1,18 @@
+import minifier from './index';
+
+const minify = minifier();
+
+/**
+ * Minify whitespace in HTML contained in a tagged template literal.
+ */
+export function html(strings: TemplateStringsArray): string {
+  let newString = '';
+
+  strings.forEach((str) => {
+    const result = minify({ content: str });
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    newString += result!.code;
+  });
+
+  return newString;
+}

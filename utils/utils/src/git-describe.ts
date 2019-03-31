4ff64b5b54bd60fe564ec74ffa3eff1ab47156b8
Max Milton <max@wearegenki.com>
2019-03-31 19:09:13 +1100

    Add new utils package

---
+++ utils/utils/src/git-describe.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line security/detect-child-process
+import { execSync } from 'child_process';
+
+/**
+ * Get the most recent git reference.
+ * @see https://git-scm.com/docs/git-describe
+ * @returns A human readable git reference.
+ */
+export function gitDescribe(): string {
+  let reference = '';
+
+  try {
+    reference = execSync('git describe --always --dirty="-dev"')
+      .toString()
+      .trim();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.warn(error);
+  }
+
+  return reference;
+}
---
+++ utils/utils/src/handle-err.ts
@@ -0,0 +1,7 @@
+/**
+ * Generic error handler for nodejs callbacks.
+ * @param err Error from node method callback.
+ */
+export function handleErr(err?: Error): void {
+  if (err) throw err;
+}

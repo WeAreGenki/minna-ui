9c0ee6c48050ce69ef67a7cf482f5b70b2f37d07
Max Milton <max@wearegenki.com>
2019-04-08 06:42:01 +1000

    Implement basic tooltip

--- components/tooltip-action/src/index.ts
+++ components/tooltip-action/src/index.ts
@@ -1,3 +1,52 @@
-export function Tooltip(): void {
-  // TODO
-}
+// TODO: Remove this type once svelte ships with official types
+// TODO: Move this to a central place
+type SvelteAction = (
+  node: HTMLElement,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  parameters?: any,
+) => {
+  destroy?: () => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  update?: (parameters: any) => void;
+};
+
+/**
+ * Tooltip Svelte action.
+ */
+export const Tooltip: SvelteAction = (node: HTMLElement, text: string) => {
+  const tooltip = document.createElement('div');
+  tooltip.className = 'tooltip';
+  tooltip.textContent = text;
+
+  function position(): void {
+    const { top, right, bottom } = node.getBoundingClientRect();
+    tooltip.style.top = `${(top + bottom) / 2}px`;
+    tooltip.style.left = `${right}px`;
+  }
+
+  function append(): void {
+    document.body.appendChild(tooltip);
+    tooltip.style.opacity = '0';
+    setTimeout(() => (tooltip.style.opacity = '1'));
+    position();
+  }
+
+  function remove(): void {
+    tooltip.remove();
+  }
+
+  node.addEventListener('mouseenter', append);
+  node.addEventListener('mouseleave', remove);
+
+  return {
+    destroy() {
+      remove();
+      node.removeEventListener('mouseenter', append);
+      node.removeEventListener('mouseleave', remove);
+    },
+
+    update(newText: string) {
+      tooltip.textContent = newText;
+    },
+  };
+};

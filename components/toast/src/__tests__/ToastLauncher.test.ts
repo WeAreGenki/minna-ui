607c2ef91e6c9fac1b9aa7d50d184d08b2020cbd
Max Milton <max@wearegenki.com>
2019-04-14 21:37:38 +1000

    Remove unused file

--- components/toast/src/__tests__/ToastLauncher.test.ts
+++
@@ -1,9 +0,0 @@
-import ToastLauncher from '../ToastLauncher.svelte';
-
-describe('ToastLauncher component', () => {
-  it('renders correctly with defaults', () => {
-    const target = document.createElement('div');
-    new ToastLauncher({ target });
-    expect(target.innerHTML).toMatchSnapshot();
-  });
-});

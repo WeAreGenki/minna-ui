27b0d520e3d0ecf2b0a750adea7166b9c00dec98
Max Milton <max@wearegenki.com>
2019-05-16 08:37:30 +1000

    Update typed ESLint rules file

--- utils/eslint-config/src/typed.ts
+++ utils/eslint-config/src/typed.ts
@@ -11,26 +11,9 @@
 
 /* eslint-disable sort-keys */
 
-/**
- * There are known performance issues with `typescript-eslint` when using type
- * info. Performance is roughly 10x slower when using the
- * `parserOptions.project` option. The parser will create an AST for each file
- * file rather than once and reuse. Follow upstream Github issue for changes.
- * @see https://github.com/typescript-eslint/typescript-eslint/issues/243
- *
- * If you have a large project with many TS files, it's recommended _not_ to
- * use this config at this point in time.
- *
- * TODO: Remove this notice comment once no longer necessary.
- */
-
-// FIXME: Need to wait until upstream issue is fixed before using this in VS
-// Code + Prettier extension with `prettier-eslint` integration
-//  ↳ https://github.com/prettier/prettier-eslint/issues/201
-
 export = {
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: process.cwd(),
   },
   rules: {

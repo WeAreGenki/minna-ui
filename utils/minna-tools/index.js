21d4c4a2a2a8f67d2eb45438fdaf2f0fe97c1d97
Max Milton <max@wearegenki.com>
2019-05-26 08:06:19 +1000

    Convert minna-tools to plain JS

---
+++ utils/minna-tools/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function __export(m) {
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+__export(require('@minna-ui/rollup-plugin-dev-server'));
+__export(require('@minna-ui/rollup-plugin-emit-css'));
+__export(require('@minna-ui/rollup-plugin-emit-html'));
+__export(require('@minna-ui/rollup-plugin-postcss'));
+__export(require('@minna-ui/rollup-plugin-purgecss'));
+__export(require('@minna-ui/utils'));
--- utils/minna-tools/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@minna-ui/rollup-plugin-dev-server';
-export * from '@minna-ui/rollup-plugin-emit-css';
-export * from '@minna-ui/rollup-plugin-emit-html';
-export * from '@minna-ui/rollup-plugin-postcss';
-export * from '@minna-ui/rollup-plugin-purgecss';
-export * from '@minna-ui/utils';

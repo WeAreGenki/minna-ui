db1cd1961d365014e88188f743e0abe56c727a87
Max Milton <max@wearegenki.com>
2019-05-28 11:21:53 +1000

    Use old school style exports

--- utils/minna-ui/index.js
+++ utils/minna-ui/index.js
@@ -1,12 +1,10 @@
 'use strict'; // eslint-disable-line
 
-/** @param {any} mod - Module exports. */
-function exportStar(mod) {
-  // @ts-ignore
-  // eslint-disable-next-line
-  for (const p in mod) if (!exports.hasOwnProperty(p)) exports[p] = mod[p];
-}
+const { html, markup, preprocess, style } = require('@minna-ui/preprocess');
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-exportStar(require('@minna-ui/preprocess'));
+module.exports = {
+  html,
+  markup,
+  preprocess,
+  style,
+};

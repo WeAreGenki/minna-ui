3804d89f78061393e9aa3afe5a0f10093684677a
Max Milton <max@wearegenki.com>
2019-05-28 12:00:39 +1000

    Ignore false positive lint error

--- utils/minna-ui/index.js
+++ utils/minna-ui/index.js
@@ -1,5 +1,6 @@
 'use strict'; // eslint-disable-line
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { html, markup, preprocess, style } = require('@minna-ui/preprocess');
 
 module.exports = {

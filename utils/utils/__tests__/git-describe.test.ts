944f1ace5575c32eafdcf42e179ae92a960ef893
Max Milton <max@wearegenki.com>
2019-06-09 16:19:25 +1000

    Add utils tests

---
+++ utils/utils/__tests__/git-describe.test.ts
@@ -0,0 +1,12 @@
+/** @jest-environment node */
+
+import { gitDescribe } from '../git-describe.js';
+
+describe('Git describe', () => {
+  it('outputs a git reference description', () => {
+    expect.assertions(2);
+    const description = gitDescribe();
+    expect(typeof description).toEqual('string');
+    expect(description).toMatch(/v\d+\.\d+\.\d+/);
+  });
+});

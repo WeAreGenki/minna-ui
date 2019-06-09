269eead534e060f3ee0b9fc3396f41beabb61dcd
Max Milton <max@wearegenki.com>
2019-06-09 22:04:03 +1000

    Refactor ESLint tests but leave comment out for now

--- utils/eslint-config/__tests__/index.test.ts
+++ utils/eslint-config/__tests__/index.test.ts
@@ -1,81 +1,38 @@
 /** @jest-environment node */
 
-// TODO: Add tests for node.js config
-
 // import { CLIEngine } from 'eslint';
+// import baseConfig from '../index.js';
+// import { bad, empty, valid } from './__fixtures__';
 
-// const linterCli = new CLIEngine({
-//   configFile: require.resolve('../index.js'),
+// const cli = new CLIEngine({
+//   baseConfig,
 //   useEslintrc: false,
 // });
 
-const sourceValid = `// eslint-disable-next-line import/no-extraneous-dependencies
-import { shout } from '@minna-ui/jest-config/fixtures/importable.js';
-
-const output = shout('test');
-
-export default output;
-`;
-const sourceInvalid = `
-'use strict';
-
-const { notAnExportedName } = require('@minna-ui/jest-config/fixtures/importable.js');
-
-const output = notAnExportedName();
-console.log(output);
-debugger;
-
-module.exports = output;
-`;
-const sourceJestValid = `/* eslint-env es6 */
-
-'use strict';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { target } = require('@minna-ui/jest-config/fixtures/importable.js');
-
-test('target', () => {
-  expect(target()).toEqual('');
-});
-`;
-const sourceJestInvalid = `
-import config from '@minna-ui/jest-config';
-
-console.log(this);
-const output = target();
-debugger;
-
-export default output;
-`;
-
-describe('ESLint config', () => {
-  it.skip('runs without linting errors on valid JS', () => {
+describe('Base ESLint config', () => {
+  it('runs on empty.js without errors', () => {
     // expect.assertions(2);
-    // const output = linterCli.executeOnText(sourceValid);
-    // expect(output.errorCount).toEqual(0);
-    // expect(output.warningCount).toEqual(0);
+    // const [name, src] = empty;
+    // const result = cli.executeOnText(src, name);
+    // expect(result.errorCount).toEqual(0);
+    // expect(result.warningCount).toEqual(0);
   });
 
-  it.skip('detects linting errors', () => {
-    // expect.assertions(2);
-    // const output = linterCli.executeOnText(sourceInvalid);
-    // expect(output.errorCount).toEqual(2);
-    // expect(output.warningCount).toEqual(2);
-  });
+  // it.each(valid)('runs on %s without errors', (name, src) => {
+  //   expect.assertions(2);
+  //   const result = cli.executeOnText(src, name);
+  //   expect(result.errorCount).toEqual(0);
+  //   expect(result.warningCount).toEqual(0);
+  // });
 
-  // FIXME: How to trigger overrides in tests?
-  describe('Jest overrides', () => {
-    it.skip('runs without linting errors on valid JS', () => {
-      // expect.assertions(2);
-      // const output = linterCli.executeOnText(sourceJestValid);
-      // expect(output.errorCount).toEqual(0);
-      // expect(output.warningCount).toEqual(0);
-    });
-
-    it.skip('detects linting errors', () => {
-      // expect.assertions(1);
-      // const output = linterCli.executeOnText(sourceJestInvalid);
-      // expect(output.errorCount).not.toEqual(0);
-    });
-  });
+  // // FIXME: Need to be more specific to avoid false positives
+  // it.each(bad)('detects linting errors on %s', (name, src) => {
+  //   expect.assertions(2);
+  //   const result = cli.executeOnText(src, name);
+  //   expect(result.errorCount).toBeGreaterThan(0);
+  //   expect(result.warningCount).toBeGreaterThan(0);
+  //   // FIXME: Expect not to contain broken rule defs, e.g.
+  //   // "message": "Definition for rule '...' was not found"
+  //   // expect(result).not.toBe(expect.arrayContaining(expect.objectContaining({ message: '' })));
+  // });
 });

ccbb18dbb3272283d35a84a117bdd1387a6c8405
Max Milton <max@wearegenki.com>
2019-06-11 06:55:38 +1000

    WIP tests

--- components/tooltip/src/__tests__/index.test.ts
+++ components/tooltip/src/__tests__/index.test.ts
@@ -1,11 +1,22 @@
-/** @jest-environment node */
-
-import { Tooltip } from '..';
+// import { tooltip } from '..';
+import BasicTooltip from './__fixtures__/BasicTooltip.svelte';
 
 describe('Tooltip', () => {
-  it('should do something', () => {
-    expect.hasAssertions();
-    expect(Tooltip).toBeDefined();
-    // TODO
+  it('renders nothing visible by default', () => {
+    expect.assertions(1);
+    const target = document.createElement('div');
+    const component = new BasicTooltip({ target });
+    // console.log('@@@@', component.$$.ctx);
+    // expect(target.innerHTML).toEqual('xx');
+    expect(target.innerHTML).toBeDefined();
+  });
+
+  it('attaches tooltip on hover', () => {
+    expect.assertions(1);
+    const target = document.createElement('div');
+    const component = new BasicTooltip({ target });
+    // console.log('@@@@', component.$$.ctx);
+    // expect(target.innerHTML).toEqual('xx');
+    expect(target.innerHTML).toBeDefined();
   });
 });

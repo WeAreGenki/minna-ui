e8cf6118fdb07b027ea242bc1bb9d0b7af424dae
Max Milton <max@wearegenki.com>
2019-06-10 18:58:31 +1000

    Refactor and clean up minna-tools

--- utils/minna-tools/index.js
+++ utils/minna-tools/index.js
@@ -1,22 +1,9 @@
-/* eslint-disable */
-
 'use strict';
 
-/** @param {any} m */
-function exportStar(m) {
-  // @ts-ignore
-  for (const p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
+/* prettier-ignore */
+// @ts-ignore
+// eslint-disable-next-line
+function exportStar(m) { for (const p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p]; }
 
 Object.defineProperty(exports, '__esModule', { value: true });
-// @ts-ignore
-exportStar(require('@minna-ui/rollup-plugin-dev-server'));
-// @ts-ignore
-exportStar(require('@minna-ui/rollup-plugin-emit-css'));
-// @ts-ignore
-exportStar(require('@minna-ui/rollup-plugin-emit-html'));
-// @ts-ignore
-exportStar(require('@minna-ui/rollup-plugin-postcss'));
-// @ts-ignore
-exportStar(require('@minna-ui/rollup-plugin-purgecss'));
 exportStar(require('@minna-ui/utils'));

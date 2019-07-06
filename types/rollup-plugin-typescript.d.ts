895a8d7cbb00a6c9c2f763c99cbb972b89186536
Max Milton <max@wearegenki.com>
2019-07-06 13:33:11 +1000

    Add rollup-plugin-typescript type declaration

--- types/rollup-plugin-typescript.d.ts
+++ types/rollup-plugin-typescript.d.ts
@@ -1,4 +1,41 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// FIXME: Remove this file once upstream PR is merged:
+// https://github.com/rollup/rollup-plugin-typescript/pull/138/files
 
-declare function rollupPluginTypeScript(options: any): any;
-export = rollupPluginTypeScript;
+import { Plugin } from 'rollup';
+import { CompilerOptionsValue, TsConfigSourceFile } from 'typescript';
+
+interface RollupTypescriptOptions {
+  /**
+   * Determine which files are transpiled by Typescript (all `.ts` and
+   * `.tsx` files by default).
+   */
+  include?: string | RegExp | readonly (string | RegExp)[] | null;
+  /**
+   * Determine which files are transpiled by Typescript (all `.ts` and
+   * `.tsx` files by default).
+   */
+  exclude?: string | RegExp | readonly (string | RegExp)[] | null;
+  /**
+   * When set to false, ignores any options specified in the config file.
+   * If set to a string that corresponds to a file path, the specified file
+   * will be used as config file.
+   */
+  tsconfig?: string | false;
+  /** Overrides TypeScript used for transpilation. */
+  typescript?: typeof import('typescript');
+  /** Overrides the injected TypeScript helpers with a custom version. */
+  tslib?: typeof import('tslib');
+
+  /** Other Typescript compiler options. */
+  /* eslint-disable @typescript-eslint/indent */
+  [option: string]:
+    | CompilerOptionsValue
+    | TsConfigSourceFile
+    | RollupTypescriptOptions['include']
+    | RollupTypescriptOptions['typescript']
+    | RollupTypescriptOptions['tslib']
+    | undefined;
+}
+
+/** Seamless integration between Rollup and Typescript. */
+export default function typescript(options?: RollupTypescriptOptions): Plugin;

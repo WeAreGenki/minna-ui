838aadd8b2785e7180e393fb679754d35eae97d6
Max Milton <max@wearegenki.com>
2019-09-06 07:14:38 +0900

    Add new minna-tools-essential package

---
+++ utils/minna-tools-essential/eslint.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  extends: ['@minna-ui/eslint-config', '@minna-ui/eslint-config/typed'],
+};
---
+++ utils/minna-tools-essential/jest-preset.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// @ts-ignore
+module.exports = require('@minna-ui/jest-config');

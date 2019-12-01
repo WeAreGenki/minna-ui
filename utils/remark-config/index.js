0aad440023f656a6f7480372d93201ba518595aa
Max Milton <max@wearegenki.com>
2019-12-01 12:50:21 +1100

    Handle frontmatter in markdown

--- utils/remark-config/index.js
+++ utils/remark-config/index.js
@@ -4,6 +4,7 @@
 
 module.exports = {
   plugins: [
+    ['remark-frontmatter', ['yaml', 'toml']],
     'remark-preset-lint-consistent',
     'remark-preset-lint-recommended',
     'remark-preset-lint-markdown-style-guide',
